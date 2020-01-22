24c7c8d56334719fba5f565a252b10378f8278f8
SDchao <1511047037@qq.com>
2020-01-22 17:46:13 +0800

    简化using

--- AutoVsCEnv_WPF/Operators/EnvChecker.cs
+++ AutoVsCEnv_WPF/Operators/EnvChecker.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Win32;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 
 namespace AutoVsCEnv_WPF.Operators
 {
