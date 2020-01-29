564f5ea6228bf4912af55e0bb38ca3f5fe518202
SDchao <1511047037@qq.com>
2020-01-29 16:18:42 +0800

    Update UpdateChecker.cs

--- AutoVsCEnv_WPF/Operators/UpdateChecker.cs
+++ AutoVsCEnv_WPF/Operators/UpdateChecker.cs
@@ -11,7 +11,7 @@ namespace AutoVsCEnv_WPF.Operators
 {
     class UpdateChecker
     {
-        private const string version = "1.0";
+        private const string version = "1.2";
         private const string checkPage = "https://raw.githubusercontent.com/SDchao/AutoVsCEnv_WPF/master/AutoVsCEnv_WPF/Operators/UpdateChecker.cs";
         public static bool HasUpdate()
         {
