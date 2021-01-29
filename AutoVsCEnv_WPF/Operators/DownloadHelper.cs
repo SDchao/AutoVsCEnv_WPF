a487f6a16e48d18fa3aa657be3b7a4559ad65224
SDchao <wentaochens@outlook.com>
2021-01-29 10:38:27 +0800

    fix ssl error while downloading

--- AutoVsCEnv_WPF/Operators/DownloadHelper.cs
+++ AutoVsCEnv_WPF/Operators/DownloadHelper.cs
@@ -22,10 +22,12 @@ namespace AutoVsCEnv_WPF.Operators
 
             Console.WriteLine(url);
             string args = "-d " + saveDirectory + " -c " +
+                "-l=\"aria2.log\" " +
                 "--header=\"accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\" " +
                 "--header=\"dnt: 1\" " +
                 "--header=\"accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6\" " +
                 "-U \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71\" " +
+                "--check-certificate=false " +
                 url;
 
             Process p = new Process();
@@ -51,6 +53,7 @@ namespace AutoVsCEnv_WPF.Operators
         private void ReceivedOutput(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine(e.Data);
+            logger.Info(e.Data);
             Regex regex = new Regex(@"\[#.*\((.+%)\) CN:1 DL:(.*) ETA:(.*)\]");
             if (e.Data != null)
             {
