76daf94ca416439f7c083046bf3f8291d40d484b
SDchao <wentaochens@outlook.com>
2021-05-19 09:53:33 +0800

    修复无法解析域名

--- AutoVsCEnv_WPF/Operators/UpdateChecker.cs
+++ AutoVsCEnv_WPF/Operators/UpdateChecker.cs
@@ -7,7 +7,7 @@ namespace AutoVsCEnv_WPF.Operators
 {
     internal class UpdateChecker
     {
-        private const string version = "1.992";
+        private const string version = "1.993";
         private const string checkPage = "https://gitee.com/SDchao/AutoVsCEnv_WPF/raw/master/AutoVsCEnv_WPF/Operators/UpdateChecker.cs";
 
         public static bool HasUpdate()
